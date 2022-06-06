9a67c193ea272ced5038493accac63411e45ca1b
Fsrexx <fabio.s.a@outlook.com>
2022-06-06 19:58:16 +0200

    Update Coll-01.py

--- naturales/Coll-01.py
+++ naturales/Coll-01.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue May 25 12:13:07 2021
-Collatz Secuencia generada por un número entero
+Collatz Secuencia generada por un número natural
 (No se buscan bucles)
 @author: fabio
 """
@@ -10,7 +10,7 @@ def Coll(n): # Función Collatz
     if n%2 == 0: return n//2
     else: return 3*n + 1
     
-n = int(input("Entrar un número entero: "))
+n = int(input("Entrar un número natural: "))
 while n != 1: # Repetir mientras n sea diferente de 1
     print(n, end=", ") # Evitar salto de línea
     n = Coll(n)
